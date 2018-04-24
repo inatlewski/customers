a800c8643782e4805fdfe3ed33547b7a434f0e70
Łukasz Inatlewski <lukasz.inatlewski@gmail.com>
2018-04-25 09:45:47 +1000

    Initial Commit. Solution structure.

---
+++ Customers.Models/Entities/AuditableEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Customers.Models.Entities
+{
+    public class AuditableEntity
+    {
+        DateTime CreatedDate;
+    }
+}
---
+++ Customers.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using System;
+using Xunit;
+
+namespace Customers.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        { 
+        }
+    }
+}
